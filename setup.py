4f2c2bbb68614c08e3966fc7beaeaf1405b0f04e
horennel <nelsonhoren@gmail.com>
2025-06-29 21:48:49 +0800

    update setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bark_python',
+    version='0.0.1',
+    description='Bark encrypted push client',
+    long_description=open('README.md', encoding='utf-8').read(),  # 读取 README 作为描述
+    long_description_content_type='text/markdown',
+    author='horennel',
+    author_email='nelsonhoren@gmail.com',
+    url='https://github.com/horennel/bark-python',
+    packages=['bark_python'],
+    install_requires=[
+        'requests>=2.25',
+        'pycryptodome>=3.18',
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Security :: Utilities :: Communications',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
