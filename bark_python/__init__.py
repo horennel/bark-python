863fdf7edecd9d39863d5c88f5c5ca9148afcbb4
horennel <nelsonhoren@gmail.com>
2025-07-03 13:54:13 +0800

    update bark-python core

--- bark_python/__init__.py
+++ bark_python/__init__.py
@@ -1,4 +1,4 @@
 from .client import Default, CBCStrategy, ECBStrategy, BarkClient, EncryptionStrategy
 
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 __all__ = ["Default", "CBCStrategy", "ECBStrategy", "BarkClient", "EncryptionStrategy"]
